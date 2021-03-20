451794a737af376831980800e8559ca4ce8cf446
mars908 <78188237+mars908@users.noreply.github.com>
2021-03-19 20:43:27 -0400

    midtermV2

--- labs/lab1/lab2/lab 3/lab4/lab5/lab6/mid/midjs/app.js
+++ labs/lab1/lab2/lab 3/lab4/lab5/lab6/mid/midjs/app.js
@@ -14,19 +14,19 @@ function draw() {
     background(50);
     //This colors the background to be a bit darker for contrast.
 
-    fill(mouseX / 5, 0, 0);
+    fill(5000 / mouseX, 0, 0);
     //this takes the current position of the mouse, and uses that number to determine how bright of a red each circle becomes.
-    circle(30, mouseY + y, mouseX / 15)
+    circle(30, mouseY + y, 9000 / mouseX);
     //draws a circle with the y coordinate being the mouse's y pos with the y variable at the top.
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
-    circle(30, mouseY + y, mouseX / 15);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
+    circle(30, mouseY + y, 9000 / mouseX);
     //Each circle also as their radius synced to the mouse's x position, divided by 15 so its not huge, but still changes size correctly.
     
     if(mouseIsPressed){
