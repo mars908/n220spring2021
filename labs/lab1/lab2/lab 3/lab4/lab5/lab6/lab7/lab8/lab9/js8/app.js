77e523c667880369502e4375ff8af0f11d2d78e9
mars908 <78188237+mars908@users.noreply.github.com>
2021-04-15 23:57:11 -0400

    updates1

--- labs/lab1/lab2/lab 3/lab4/lab5/lab6/lab7/lab8/lab9/js8/app.js
+++ labs/lab1/lab2/lab 3/lab4/lab5/lab6/lab7/lab8/lab9/js8/app.js
@@ -1,4 +1,5 @@
 let addNum = document.getElementById("addNum");
+//this grabs the number the user types from the html and allows me to use it in the split array later
 
 function findSum() {
 
@@ -9,7 +10,7 @@ function findSum() {
     let nums = 0;
 
     let sum = 0;
-
+    //making two variables lets me have 1 variable for the average, and one for the sum
     for(let i = 0; i < myArray.length; i++) {
         nums += Number(myArray[i]);
         sum += Number(myArray[i]);
@@ -19,4 +20,5 @@ function findSum() {
     console.log(average);
     console.log(sum);
     addNum.value = "";
+    //this bottom section actually does the dividing for the average, and displays them to the log.
 }
--- labs/lab1/lab2/lab 3/lab4/lab5/lab6/lab7/lab8/lab9/js8/app2.js
+++ labs/lab1/lab2/lab 3/lab4/lab5/lab6/lab7/lab8/lab9/js8/app2.js
@@ -11,12 +11,13 @@ function wordSearch() {
     let wordTwo = "water";
 
     let wordThree = "tire";
+    // these three variables are made to check for if the user types in one of the bad words
 
     for(let i = 0; i < splitSentence.length; i++){
         if(splitSentence[i] == wordOne, wordTwo, wordThree) {
             console.log("found " + i +  " words");
         }
     }
-
+    //the if statements runs the typed array variables through the loop, and if any variables matchs the bad word variables, it reports it, and how many it found.
     findWord.value = "";
 }
